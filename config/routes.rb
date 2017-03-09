a87f8f2ddf2e173098de7ac4563b811a392df89e
Gedeon Ntwali <ntwari28@gmail.com>
2017-03-09 12:23:16 +0200

    I added payments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlto
   get "/", to: 'pages#welcome'
+  get "payments", to:'payments#index'
+  get "payments/new", to: 'payments#new'
 end
